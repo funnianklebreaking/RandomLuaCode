99d431ce0b4968ac687e827d3adda913013f50bd
funnianklebreaking <147280933+funnianklebreaking@users.noreply.github.com>
2023-10-14 00:40:11 -0300

    Update funnijump.lua

--- funnijump.lua
+++ funnijump.lua
@@ -1,17 +1,25 @@
-local character = game.Players.LocalPlayer.Character
-local humanoid = character:WaitForChild("Humanoid")
+local player = game.Players.LocalPlayer
 
-local groundSpeed = 24
-local airSpeed = 50
+local function onCharacterAdded(character)
+    local humanoid = character:WaitForChild("Humanoid")
+    local groundSpeed = 24
+    local airSpeed = 50
 
-humanoid.WalkSpeed = groundSpeed
+    humanoid.WalkSpeed = groundSpeed -- Set the initial ground speed here.
 
-local function onStateChanged(oldState, newState)
-    if newState == Enum.HumanoidStateType.Jumping or newState == Enum.HumanoidStateType.Freefall then
-        humanoid.WalkSpeed = airSpeed
-    else
-        humanoid.WalkSpeed = groundSpeed
+    local function onStateChanged(oldState, newState)
+        if newState == Enum.HumanoidStateType.Jumping or newState == Enum.HumanoidStateType.Freefall then
+            humanoid.WalkSpeed = airSpeed
+        else
+            humanoid.WalkSpeed = groundSpeed
+        end
     end
+
+    humanoid.StateChanged:Connect(onStateChanged)
 end
 
-humanoid.StateChanged:Connect(onStateChanged)
+player.CharacterAdded:Connect(onCharacterAdded)
+
+if player.Character then
+    onCharacterAdded(player.Character)
+end
